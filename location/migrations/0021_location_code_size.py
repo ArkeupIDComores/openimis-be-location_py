0dcedc9e6d6d7cad45c6312b531d8229b0fb4bca
maxime ngoe <maxime.ngoe@gmail.com>
2025-02-03 11:33:56 +0100

    Fix on Migration to manage View

--- location/migrations/0021_location_code_size.py
+++ location/migrations/0021_location_code_size.py
@@ -12,5 +12,69 @@ class Migration(migrations.Migration):
             model_name='location',
             name='code',
             field=models.CharField(db_column='LocationCode', max_length=50),
-        )
+        ),
+
+        migrations.RunSQL(
+            """
+            -- Recrée la vue "uvwAmountRejected" pour les montants rejetés
+            CREATE OR REPLACE VIEW public."tblDistricts"
+            AS
+            SELECT "tblLocations"."LocationId" AS "DistrictId",
+                "tblLocations"."LocationCode" AS "DistrictCode",
+                "tblLocations"."LocationName" AS "DistrictName",
+                "tblLocations"."ParentLocationId" AS "Region",
+                "tblLocations"."ValidityFrom",
+                "tblLocations"."ValidityTo",
+                "tblLocations"."LegacyID",
+                "tblLocations"."AuditUserId",
+                "tblLocations"."RowId"
+            FROM "tblLocations"
+            WHERE "tblLocations"."ValidityTo" IS NULL AND "tblLocations"."LocationType"::bpchar = 'D'::bpchar;
+
+            CREATE OR REPLACE VIEW public."tblRegions"
+            AS
+            SELECT "tblLocations"."LocationId" AS "RegionId",
+                "tblLocations"."LocationCode" AS "RegionCode",
+                "tblLocations"."LocationName" AS "RegionName",
+                "tblLocations"."ValidityFrom",
+                "tblLocations"."ValidityTo",
+                "tblLocations"."LegacyID",
+                "tblLocations"."AuditUserId",
+                "tblLocations"."RowId"
+            FROM "tblLocations"
+            WHERE "tblLocations"."ValidityTo" IS NULL AND "tblLocations"."LocationType"::bpchar = 'R'::bpchar;
+
+            CREATE OR REPLACE VIEW public."tblVillages"
+            AS
+            SELECT "tblLocations"."LocationId" AS "VillageId",
+                "tblLocations"."ParentLocationId" AS "WardId",
+                "tblLocations"."LocationCode" AS "VillageCode",
+                "tblLocations"."LocationName" AS "VillageName",
+                "tblLocations"."MalePopulation",
+                "tblLocations"."FemalePopulation",
+                "tblLocations"."OtherPopulation",
+                "tblLocations"."Families",
+                "tblLocations"."ValidityFrom",
+                "tblLocations"."ValidityTo",
+                "tblLocations"."LegacyID",
+                "tblLocations"."AuditUserId"
+            FROM "tblLocations"
+            WHERE "tblLocations"."ValidityTo" IS NULL AND "tblLocations"."LocationType"::bpchar = 'V'::bpchar;
+
+            CREATE OR REPLACE VIEW public."tblWards"
+            AS
+            SELECT "tblLocations"."LocationId" AS "WardId",
+                "tblLocations"."ParentLocationId" AS "DistrictId",
+                "tblLocations"."LocationCode" AS "WardCode",
+                "tblLocations"."LocationName" AS "WardName",
+                "tblLocations"."ValidityFrom",
+                "tblLocations"."ValidityTo",
+                "tblLocations"."LegacyID",
+                "tblLocations"."AuditUserId"
+            FROM "tblLocations"
+            WHERE "tblLocations"."ValidityTo" IS NULL AND "tblLocations"."LocationType"::bpchar = 'W'::bpchar;
+
+
+            """
+        ),
     ]
